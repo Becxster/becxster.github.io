bef0f4a84aa4b7b4940b04ffb4ae7cbf00741ae7
Becxster <beccstah5@gmail.com>
2024-07-19 00:03:58 -0300

    added nav bar Projects dropdown menu and project spotlight on home page

--- script.js
+++ script.js
@@ -2,3 +2,42 @@ function toggleMenu() {
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.toggle('show');
 }
+
+function redirectToPage(url) {
+  window.location.href = url;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const hoverBox = document.getElementById('hoverBox');
+  const dropdownMenu = document.getElementById('dropdownMenu');
+
+  hoverBox.addEventListener('mouseover', function() {
+    // Get the position of the hoverBox
+    const rect = hoverBox.getBoundingClientRect();
+
+    // Set the position of the dropdownMenu
+    dropdownMenu.style.top = rect.bottom + 'px';
+    dropdownMenu.style.left = rect.left + 'px';
+  });
+
+  
+
+  hoverBox.addEventListener('mouseleave', function() {
+    if (window.innerWidth > 900) {
+    // Hide the dropdown menu when not hovering
+    dropdownMenu.style.display = 'none';
+    }
+    else {
+    }
+  });
+
+  hoverBox.addEventListener('mouseover', function() {
+    if (window.innerWidth > 900) {
+    // Show the dropdown menu when hovering
+    dropdownMenu.style.display = 'block';
+    }
+    else {
+    dropdownMenu.style.display = 'none'
+    }
+  });
+});
